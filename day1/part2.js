f1afa153af74fce3c649ad41678e78951b425732
izexi <cr139lvl1@gmail.com>
2018-12-03 21:53:26 +0000

    ;

--- day1/part2.js
+++ day1/part2.js
@@ -1,7 +1,7 @@
 const { readFileSync } = require("fs");
 const inpArr = readFileSync("input.txt").toString().split('\n');
 const totals = new Set();
-let total = 0
+let total = 0;
 let i = 0;
 
 while (!totals.has(total)) {
