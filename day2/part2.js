f3b1fc1035848dc8e70bce7b828a301b0dcaf5bb
izexi <cr139lvl1@gmail.com>
2018-12-03 21:49:03 +0000

    day 3 |  part 1 (node.js)

--- day2/part2.js
+++ day2/part2.js
@@ -3,8 +3,7 @@ const inpArr = readFileSync("input.txt").toString().split('\n');
 
 for (let i = 0; i < inpArr.length; i++) {
     for (let j = inpArr.length - 1; j >= 0; j--) {
-        const prev = [...inpArr[i]];
-        const curr = [...inpArr[j]];
+        const [prev, curr] = [[...inpArr[i]], [...inpArr[j]]];
         const diffLetters = prev.filter((l, i) => curr[i] !== l)
         if (diffLetters.length === 1) {
             return console.log(`Day 2 | Part 2 - Solution: ${prev.filter(l => l != diffLetters).join("")}`);
---
+++ day3/part1.js
@@ -0,0 +1,20 @@
+const { readFileSync } = require("fs");
+const inpArr = readFileSync("input.txt").toString().split('\n');
+const claimed = new Map();
+let overlap = 0;
+
+inpArr.forEach(claim => {
+    const measurements = claim.match(/@ (\d+),(\d+)\: (\d+)x(\d+)/).map(Number);
+    const [startingX, startingY] = [measurements[1], measurements[2]];
+    const [width, height] = [measurements[1] + measurements[3], measurements[2] + measurements[4]];
+
+    for (let i = startingX; i < width; i++) {
+        for (let j = startingY; j < height; j++) {
+            const coords = `${i}, ${j}`;
+            claimed.set(coords, (claimed.get(coords) || 0) + 1);
+            if (claimed.get(coords) === 2) overlap++;
+        }
+    }
+});
+
+console.log(`Day 3 | Part 1 - Solution: ${overlap}`);
